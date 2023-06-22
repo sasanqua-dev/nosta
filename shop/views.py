c01db93210ef5ac995e9f84c5ff50837db21638b
sasanqua-create <78304794+sasanqua-create@users.noreply.github.com>
2023-06-22 17:13:49 +0900

    fix

--- shop/views.py
+++ shop/views.py
@@ -37,7 +37,7 @@ def register(request):
             sic=sic,
             category=category
         )
-        return redirect('shop:home',shop.id)
+        return redirect('shop:home',shop.code)
 
     else:
         return render(request, 'shop/register.html')
