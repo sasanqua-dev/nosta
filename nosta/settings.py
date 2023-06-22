8b6f8f74bd09bde7f48c43574715a7ee1723c985
sasanqua-create <78304794+sasanqua-create@users.noreply.github.com>
2023-06-22 16:50:28 +0900

    fix

--- nosta/settings.py
+++ nosta/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fg+@^71lbdwcl@5(6wil#b792q83f)i%s)848497h7x!i^xce&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','nosta.prasic-plus.com']
 
 
 # Application definition
