b089d2c0a84a2045c93c4bd70a076282d53a11c8
sasanqua-create <78304794+sasanqua-create@users.noreply.github.com>
2023-07-26 11:32:09 +0900

    fix

--- static/js/regi/app.js
+++ static/js/regi/app.js
@@ -108,7 +108,6 @@ SQR.modal = (() => {
         SQR.reader.findQR();
     };
     (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
-        const $target = $close.closest('.modal');
         $close.addEventListener('click', () => {
             close();
         });
