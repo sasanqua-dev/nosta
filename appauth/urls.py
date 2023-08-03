5e9f73b682dcc2795a216fee1711116ebc97a63a
sasanqua-create <78304794+sasanqua-create@users.noreply.github.com>
2023-08-04 00:32:17 +0900

    ゲストアカウントの実装 細かなバグ修正

--- appauth/urls.py
+++ appauth/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     re_path(r'^login/', views.user_login, name='login'),
     re_path(r'^logout/', auth_views.LogoutView.as_view(), name='logout'),
     re_path(r'^register/', views.user_register, name='register'),
+    re_path(r'^register_guest/', views.user_register_guest, name='guest_register'),
     re_path(r'^social-auth/', include('social_django.urls', namespace='social')),
     path('service/<str:shopCODE>', views.service, name='service'),
     path('', views.home, name='home'),
