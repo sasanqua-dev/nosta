0d9be5444306b4edc7fb69f73c12996ce36bff45
sasanqua-create <78304794+sasanqua-create@users.noreply.github.com>
2023-08-06 03:38:47 +0900

    fix

--- nosta/settings/development.py
+++ nosta/settings/development.py
@@ -87,8 +87,6 @@ TEMPLATES = [
     },
 ]
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
 WSGI_APPLICATION = 'nosta.wsgi.application'
 
 
@@ -145,7 +143,9 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/assets")
 # 画像を保存する先の指定
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
--- nosta/settings/production.py
+++ nosta/settings/production.py
@@ -92,8 +92,6 @@ TEMPLATES = [
     },
 ]
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
 WSGI_APPLICATION = 'nosta.wsgi.application'
 
 
@@ -163,7 +161,10 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/assets")
+
 # 画像を保存する先の指定
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
